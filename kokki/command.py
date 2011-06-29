e6d737706e8964df1f7ce7b022ab5343df820a11
lechat <ctpeko3a@gmail.com>
2011-06-29 23:22:35 +0200

    Added verbose messages Added error message if kitchen.py is not found Added error message if config file format is not yaml or pickle

--- kokki/command.py
+++ kokki/command.py
@@ -32,19 +32,29 @@ def main():
         logger.setLevel(logging.DEBUG)
 
     if options.config:
+        logger.debug('Config file specified as %s' % options.config)
         if ':' in options.config:
             fmt, filename = options.config.split(':', 1)
         else:
+            logger.debug('Assuming that config is in yaml format')
             fmt, filename = "yaml", options.config
         if fmt == "yaml":
+            logger.debug(msg='Config file format is yaml')
             import yaml
             with open(options.config, "rb") as fp:
                 kit = yaml.load(fp.read())
         elif fmt == "pickle":
+            logger.debug(msg='Config file format is pickle')
             import cPickle as pickle
             with open(filename, "rb") as fp:
                 kit = pickle.load(fp)
+        else: 
+            sys.stderr.write("Unknown config format specified '%s'. Can only work with yaml or pickle \n" % c)
+            sys.exit(1)
+                
+            
     else:
+        logger.debug('Config file not specified, trying to read "kitchen.py"')
         path = os.path.abspath(options.filename)
         if not os.path.isdir(path):
             path = os.path.dirname(path)
@@ -56,13 +66,22 @@ def main():
         else:
             files = [options.filename]
 
+        logger.debug('Processing %s as kitchen file(s)' % files)
+
         globs = {}
         for fname in files:
             globs["__file__"] = os.path.abspath(fname)
-            with open(fname, "rb") as fp:
-                source = fp.read()
-            exec compile(source, fname, 'exec') in globs
-        del globs['__file__']
+            if os.path.exists(globs["__file__"]):
+                file_found = True
+                with open(fname, "rb") as fp:
+                    logger.debug('Compiling %s' % fname)
+                    source = fp.read()
+                    exec compile(source, fname, 'exec') in globs
+                del globs['__file__']
+        
+        if not file_found:
+            sys.stderr.write("Need to have 'kitchen.py' or other files specified by -f parameter")
+            sys.exit(1)
 
         kit = Kitchen()
         roles = []
@@ -75,6 +94,7 @@ def main():
         for r in roles:
             r(kit)
     
+    logger.debug('Processing overrides: %s' % options.overrides)
     for over in options.overrides:
         name, value = over.split('=', 1)
         try:
@@ -84,12 +104,14 @@ def main():
         kit.update_config({name: value})
     
     if options.dump:
+        logger.debug('Dumping config files')
         if ':' in options.dump:
             fmt, filename = options.dump.split(':', 1)
         else:
+            logger.debug('Assuming format is yaml')
             fmt, filename = "yaml", options.dump
         if fmt == "yaml":
-            import yaml
+            import yaml 
             if filename == "-":
                 print yaml.dump(kit)
             else:
@@ -102,8 +124,13 @@ def main():
             else:
                 with open(filename, "wb") as fp:
                     pickle.dump(kit, fp, pickle.HIGHEST_PROTOCOL)
+        else: 
+            sys.stderr.write("Unknown config format specified '%s'. Can only work with yaml or pickle \n" % c)
+            sys.exit(1)
+
         sys.exit(0)
 
+    logger.debug('Configuration is done. Visiting kitchen.')
     kit.run()
 
 if __name__ == "__main__":
